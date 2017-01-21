b5328ce08426245fa8cc1135f45aa40ffd70337d
unalfaruk <unalfaruk@outlook.com>
2017-01-21 12:22:14 +0300

    Get tags and contents from web site with BeautifulSoup4

--- sozBotu.py
+++ sozBotu.py
@@ -1,7 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
 
 url="http://guzelsozlerblog.com/gunun-sozu"
 
 response=requests.get(url)
 
-print(response.text)
+AnalizEdilecekIcerik=response.text #get HTML code of URL
+
+icerigiAl=BeautifulSoup(AnalizEdilecekIcerik,"lxml") #read and analyze HTML code of url
+#we use "lxml" because, when it wasn't there, there was an error. Code was running but we don't want to see warning :)
+#warning text said this parameter to me. You can read this URL for parameters: https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
+
+filtre=icerigiAl.find_all(id="PageContent") #Filter HTML code which analyzed for just div tags
+print(filtre)
+print(filtre[0])
