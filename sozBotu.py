b506984ca9eb4e5dbc748a323cffc947e4f68631
unalfaruk <unalfaruk@outlook.com>
2017-01-21 13:05:38 +0300

    Parse tags,contents and print with iteration

--- sozBotu.py
+++ sozBotu.py
@@ -1,9 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-url="http://guzelsozlerblog.com/gunun-sozu"
+url="http://guzelsozlerblog.com/gunun-sozu" #Our url which will we read
 
-response=requests.get(url)
+response=requests.get(url) #Go to URL
+response.encoding= "iso-8859-9" #Web site's charset --> <meta http-equiv="content-type" content="text/html; charset=iso-8859-9">
+#Encoding is important for your text's characters which get from website
 
 AnalizEdilecekIcerik=response.text #get HTML code of URL
 
@@ -11,6 +13,8 @@ icerigiAl=BeautifulSoup(AnalizEdilecekIcerik,"lxml") #read and analyze HTML code
 #we use "lxml" because, when it wasn't there, there was an error. Code was running but we don't want to see warning :)
 #warning text said this parameter to me. You can read this URL for parameters: https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
 
-filtre=icerigiAl.find_all(id="PageContent") #Filter HTML code which analyzed for just div tags
-print(filtre)
-print(filtre[0])
+#Filter HTML code which analyzed with CSS selector for just div tags
+sozler=icerigiAl.select("div#PageContent") #get div containers
+
+for i in range(0,len(sozler)): #iterate from 0 to length of our list
+    print(sozler[i].contents[0].text +": "+sozler[i].contents[2]+"\n") #Parse tags' contents and print
